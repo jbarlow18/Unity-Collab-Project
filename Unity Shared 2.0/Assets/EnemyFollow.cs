4e70f9c1977677302efe3efbcdb0f398b95d151a
TRasmusson <119694864+TRasmusson@users.noreply.github.com>
2023-04-20 17:19:01 -0400

    Trying to fix animations (failing)

--- Unity Shared 2.0/Assets/EnemyFollow.cs
+++ Unity Shared 2.0/Assets/EnemyFollow.cs
@@ -6,15 +6,18 @@ public class NewBehaviourScript : MonoBehaviour
 {
     public NavMeshAgent enemy;
     public Transform Player;
+    float horizontalMove = 0f;
+    public float runSpeed = 4f;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    
     // Update is called once per frame
     void Update()
     {
+        
         enemy.SetDestination(Player.position);
     }
 }
