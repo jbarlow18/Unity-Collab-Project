cebf649018754530a49650fa5f8829833f3ecf30
Caleb <clc05@comcast.net>
2023-04-21 12:52:12 -0400

    Random Changes

--- Unity Shared 2.0/Assets/Spawning.cs
+++ Unity Shared 2.0/Assets/Spawning.cs
@@ -8,9 +8,9 @@ public class RandomSpawner : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space))
         {
-            Vector3 randomSpawnPosition = new Vector3(Random.Range(-10, 11), 5, Random.Range(-10, 11));
+            Vector3 randomSpawnPosition = new Vector3(Random.Range(0, 100), 24, Random.Range(0, 110));
             Instantiate(cubePrefab, randomSpawnPosition, Quaternion.identity);
         }
     }
