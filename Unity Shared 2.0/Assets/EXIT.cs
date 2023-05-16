38b13f2f24ef33289ee7e81612abeff5412a381a
Caleb <clc05@comcast.net>
2023-05-16 15:43:32 -0400

    Exit and Logos

---
+++ Unity Shared 2.0/Assets/EXIT.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EXIT : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+    public void QuitGame()
+    {
+    }
+}
